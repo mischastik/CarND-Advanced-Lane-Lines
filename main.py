bcff35633fd3a8997c090099b2f702a5ea7cb729
mischastik <michael.balda@gmail.com>
2017-10-27 23:08:11 +0200

    Add video processing.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from laneDetection import LaneTracker
 from tools import pipeline
 import matplotlib.pyplot as plt
 from imageTransformation import calibrate_camera
+from moviepy.editor import VideoFileClip
 
-### PART 1: Calibrate the camera
+### Calibrate the camera
 
 ret, mtx, dist = calibrate_camera()
 
@@ -14,7 +15,11 @@ imgRGB = img[:,:,::-1]
 #plt.imshow(imgRGB)
 #plt.show()
 laneTracker = LaneTracker(mtx, dist)
-augmented_image = laneTracker.process_image(imgRGB)
+augmented_image = laneTracker.process_image(img)
 print(augmented_image.shape)
 plt.imshow(augmented_image)
 plt.show()
+
+clip1 = VideoFileClip("./project_video.mp4")
+res_video = clip1.fl_image(laneTracker.process_image) #NOTE: this function expects color images!!
+res_video.write_videofile("./project_video_res.mp4", audio=False)
